2d1ab9d8323528a9c31549df19e6d191f1a61625
phumipatc <phumipat.c@outlook.com>
2021-10-09 21:22:15 +0700

    SubjectList page

---
+++ src/pages/SubjectList.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import Loading from "../components/Loading";
+
+function SubjectList() {
+  const [IsLoading, SetIsLoading] = useState(true);
+  const [SubjectList, SetSubjectList] = useState();
+  const API_Link = "";
+
+  useEffect(() => {
+    // fetch(API_Link)
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     SetIsLoading(false);
+    //     SetSubjectList(data);
+    //   });
+  }, []);
+
+  if (IsLoading) {
+    return <Loading />;
+  }
+
+  return <div></div>;
+}
+
+export default SubjectList;
