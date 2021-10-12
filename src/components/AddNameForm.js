6fc8139a8fe7de0d86cc0cffd546e5f04ed0da1f
kyrov <p50445.2013@gmail.com>
2021-10-12 16:29:47 +0700

    Part 2

---
+++ src/components/AddNameForm.js
@@ -0,0 +1,34 @@
+import classes from "./AddNameForm.module.css";
+import DocInput from "./DocInput";
+import { useRef } from "react";
+import { Redirect, history, useHistory, location } from "react-router";
+function AddNameForm(props) {
+  const NameInputRef = useRef();
+  const history = useHistory();
+  function Click_Handler(event) {
+    event.preventDefault();
+    const NameInputValue = NameInputRef.current.value;
+    const DocData = {
+      DocLink: props.link,
+      DocName: NameInputValue,
+    };
+    console.log(DocData);
+  }
+  return (
+    <form>
+      <div>
+        <DocInput
+          label="File name"
+          type="text"
+          id="DocName"
+          required={true}
+          refer={NameInputRef}
+        />
+      </div>
+      <div>
+        <button onClick={Click_Handler}>choose the selected file</button>
+      </div>
+    </form>
+  );
+}
+export default AddNameForm;
